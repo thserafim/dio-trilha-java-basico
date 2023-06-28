591d3754222e6f6f383226b5d0a5692e78627223
thserafim <thiagoseraffim@hotmail.com>
2023-06-28 15:25:02 -0300

    Exemplo de exercicios

---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,26 @@
+public class operadores {
+//     public static void main (String args []){
+//         int numero = 5;
+
+//         while (numero >= 5)
+
+//         System.out.println( numero);
+//    }  
+
+    //   public static void main (String args [] ){
+    //     int a,b;
+
+    //     a = 5;
+    //     b = 6;
+
+    //     String resultado = a == b ? "Verdadeiro" : "Falso";
+
+    //     System.out.println(resultado);    
+    //   }
+    public static void main(String args []){
+        String nomeUm = "GLEYSON";
+        String nomeDois = new String ("GLEYSON");
+
+        System.out.println(nomeUm.equals(nomeDois));
+    }
+}
