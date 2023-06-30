ab7268e130050a0a232ba856d7980e4737309141
thserafim <thiagoseraffim@hotmail.com>
2023-06-30 12:15:19 -0300

    Projeto final conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main (String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Bem vindo ao Banco Dio\n");
+
+        System.out.println("Digite o número da conta: ");
+        int numeroConta = teclado.nextInt();
+        teclado.nextLine();
+        System.out.println("Digite o número da agência: ");
+        String numeroAgencia = teclado.nextLine();
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = teclado.nextLine();
+        System.out.println("Digite o valor do depósito inicial: ");
+        double saldoInicial = teclado.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " sua conta foi criada com sucesso! \n Sua agência é: " + numeroAgencia + ", sua conta é: " + numeroConta + ", e seu saldo inicial é: " + saldoInicial);
+       
     }
 }
